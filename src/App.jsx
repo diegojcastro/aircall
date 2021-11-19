540b611e9a1e07c14106fb17a54fcf4d50958db5
Diego Castro <diegojcr4@gmail.com>
2021-11-19 09:56:20 -0500

    add navbar template

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,23 @@ const App = () => {
     <div className='container'>
       <Header/>
       <div className="container-view">Some activities should be here</div>
+
+
+      <div className="card">
+        <div className="card-header">
+          Quote
+        </div>
+        <div className="card-body">
+          <blockquote className="blockquote mb-0">
+            <p>A well-known quote, contained in a blockquote element.</p>
+            <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+          </blockquote>
+        </div>
+      </div>
+
     </div>
+
+    
   );
 };
 
