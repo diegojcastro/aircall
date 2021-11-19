168cec226713c6a4c8645e639d050e41a0bf945d
Diego Castro <diegojcr4@gmail.com>
2021-11-19 17:33:41 -0500

    add icons to call list items

--- src/CallListItem.jsx
+++ src/CallListItem.jsx
@@ -12,10 +12,16 @@ export default function CallListItem(props) {
       </div>
       <div className="card-body list-item-container">
         <div className="list-item-details-left">
-          [icon]
+          <div className="phone-icon">
+            {props.call_type==='answered' && <i class="fas fa-phone"></i>}
+            {props.call_type==='missed' && <i class="fas fa-phone-slash"></i>}
+            {props.call_type==='voicemail' && <i class="fas fa-voicemail"></i>}
+          </div>
           <div className="list-item-details-inner">
             <p>{props.from}</p>
-            <p className="card-subtitle text-muted">tried to call on {props.to}</p>
+            {props.call_type==='missed' && <p className="card-subtitle call-receiver text-muted">tried to call on {props.to}</p>}
+            {props.call_type==='answered' && <p className="card-subtitle call-receiver text-muted">for {props.to}</p>}
+            {props.call_type==='voicemail' && <p className="card-subtitle call-receiver text-muted">voicemail for {props.to}</p>}
           </div>
         </div>
         <div className="list-item-details-right">
